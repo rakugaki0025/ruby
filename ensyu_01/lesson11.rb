f937cf713f37d82e84b2c608d713f30475280315
rakugaki0025 <135841624+rakugaki0025@users.noreply.github.com>
2023-06-24 10:26:45 +0000

    2-11 finish

--- ensyu_01/lesson11.rb
+++ ensyu_01/lesson11.rb
@@ -6,21 +6,21 @@ class Car                                   # 始
   end
 end                                         # 終
 
-class Bus < Car                             # 継承,子 < 親
+class Track < Car                           # 継承,子 < 親
   def run(distance)                         # run,文章に代入する項目(変更するところ)
     super                                   # 子クラスのメソッド内で定義すると、
                                             # 親クラス内にある同じ名前のメソッドを
                                             # 呼び出すことができます。
-    puts "30人を乗せて,走っています。"      # 文字列,ダブル,出力
+    puts "大きな荷物を乗せて走っています。" # 文字列,ダブル,出力
   end
 end                                         # 終
 
-bus = Bus.new                               # 1項目につき 1 class ?
+track = Track.new                               # 1項目につき 1 class ?
                                             #  変数 = インスタンス
                                             #  Bus = Bus.new
-bus.run(5)                                  # distance,代入(引数)
+track.run(5)                                  # distance,代入(引数)
 
 
 
-puts Bus.superclass                        # 継承先確認
+puts Track.superclass                        # 継承先確認
 
