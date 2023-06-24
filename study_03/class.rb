c244c48d94b362da6dc9adc22c06b1a81076a3db
rakugaki0025 <135841624+rakugaki0025@users.noreply.github.com>
2023-06-24 08:00:00 +0000

    2-9-1 totyu

---
+++ study_03/class.rb
@@ -0,0 +1,7 @@
+array = [1,2,3]      # 整数,
+puts array.class     # 出力クラス
+puts array.join("*") # 連結した文字列を繰り返す
+
+string = "Hello"     # 文字列,ダブル
+puts string.class    # 出力クラス
+puts string.join("*") # 連結した文字列を繰り返す
